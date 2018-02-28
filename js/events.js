cf6229f1984a1cd5985b53a60ac3f68507b84507
Bryan Williams <brywilldev@gmail.com>
2018-02-28 03:07:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,16 @@
 function getIt() {
-  $('p').on("click", function() {
+  $('p').on('click', function() {
     alert("Hey!");
   });
 }
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
 $(document).ready(function(){
   getIt();
+  frameIt();
 });
